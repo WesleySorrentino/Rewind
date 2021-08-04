ab1263b249aa7ac93d762479a18f05eb143bc276
Wesley Sorrentino <wes.james17@gmail.com>
2021-08-04 15:59:50 -0400

    Inital Commit

--- JS/site.js
+++ JS/site.js
@@ -1,4 +1,30 @@
-//Call Hello World
-function helloWorld() {
-    alert('hello world');
-};
+// Get the string from the page
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+
+    displayString(revString)
+
+}
+
+// Reverse the String object
+function reverseString(userString){
+    let revString = [];
+
+    // reverse string
+    for (let i = userString.length - 1; i >= 0; i--) {
+
+        revString += userString[i];        
+    }
+    return revString;
+}
+
+// Display results
+function displayString(reverseString){
+    // Write message to page
+    document.getElementById("message").innerHTML = `Your string reversed is: ${reverseString}`
+    // Show alert box
+    document.getElementById("alert").classList.remove("invisible");
+}
